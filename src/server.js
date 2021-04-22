a43871c76a1c2bfe900fb812f61546de4b4b0ead
Tyler Ivie <59585410+tivie001@users.noreply.github.com>
2021-04-22 14:10:47 -0600

    Moved build back

--- src/server.js
+++ src/server.js
@@ -1,17 +1,12 @@
 const { ApolloServer } = require('apollo-server')
 const { schema } = require('./schema')
 const { context } = require('./context')
-const path = require('path')
 
 const server = new ApolloServer({
   schema: schema,
-  context: context,
-  introspection: true
+  context: context
 })
-app.use(express.static('public'))
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-})
+
 server.listen(process.env.PORT || 4000).then(async ({ url }) => {
   console.log(`\
 🚀 Server ready at: ${url}
