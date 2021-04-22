4390cfa94c48ddd88ce84021e0eb076abe7f47c1
Tyler Ivie <59585410+tivie001@users.noreply.github.com>
2021-04-22 12:37:35 -0600

    changed apollo url.

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -10,7 +10,7 @@
   export let url = ''
 
   const client = new ApolloClient({
-    uri: '/graphql',
+    uri: 'https://docker-watchly-app.herokuapp.com/graphql',
 
     onError: ({ networkError, graphQLErrors }) => {
       console.log('graphQLErrors', graphQLErrors)
